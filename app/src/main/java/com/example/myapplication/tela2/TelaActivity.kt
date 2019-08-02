d26b29d57772ed0036f27c25362a040f6333bc04
Artur Pedrosa <maverickanp@gmail.com>
2019-08-02 15:35:01 -0300

    criacao da segunda tela

---
+++ app/src/main/java/com/example/myapplication/tela2/TelaActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication.tela2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class TelaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
